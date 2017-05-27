d5775a1484a9b134cf60ac21039d92effa06affd
Robert <balys.robert@gmail.com>
2017-05-27 13:26:21 +0200

    checked internet connection, password and email vaild

--- app/src/main/java/com/sda/balys/robert/gameofcards/Utils.java
+++ app/src/main/java/com/sda/balys/robert/gameofcards/Utils.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.content.Context;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.widget.Toast;
 
 /**
  * Created by RENT on 2017-05-27.
@@ -11,23 +12,43 @@ import android.net.NetworkInfo;
 
 public class Utils {
 
-    public static boolean isEmailValid (String email){
+    //metoda showToast wyswietla toasta w szybkim tepie
+    public static void showToast(Context context, String msg) {
+        Toast.makeText(context, msg, Toast.LENGTH_SHORT).show();
+    }
+    //sprawdzamy czy email uzytkownika zawiera znak @
+    public static boolean isEmailVavlid(String email) {
         return email.contains("@");
     }
-    public static boolean isPasswordLongCorrect(String password) {
-        if (password.length() > 6) {
-            return true;
-        }
-        return false;
+    //sprawdzamy czy uzytkownik przy hasle podał ponad 6 znaków
+    public static boolean isPasswordValid(String password) {
+        return password.length() > 6;
     }
+    //sprawdzanie czy posiadamy dostep do internetu
+    public static boolean checkInternetConnection(Context context) {
+        ConnectivityManager cm =
+                (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+        return netInfo != null && netInfo.isConnectedOrConnecting();
+    }
+
+//    public static boolean isEmailValid (String email){
+//        return email.contains("@");
 //    }
-    public static boolean isInternetConnectionCorrect(Context context){
-
-            ConnectivityManager connectivityManager
-                    = (ConnectivityManager) context.getSystemService (Context.CONNECTIVITY_SERVICE);
-            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
-            return activeNetworkInfo != null && activeNetworkInfo.isConnected();
-
-    }
+//    public static boolean isPasswordLongCorrect(String password) {
+//        if (password.length() > 6) {
+//            return true;
+//        }
+//        return false;
+//    }
+////    }
+//    public static boolean isInternetConnectionCorrect(Context context){
+//
+//            ConnectivityManager connectivityManager
+//                    = (ConnectivityManager) context.getSystemService (Context.CONNECTIVITY_SERVICE);
+//            NetworkInfo activeNetworkInfo = connectivityManager.getActiveNetworkInfo();
+//            return activeNetworkInfo != null && activeNetworkInfo.isConnected();
+//
+//    }
 
 }
--- app/src/main/java/com/sda/balys/robert/gameofcards/presenters/RegisterPresenter.java
+++ app/src/main/java/com/sda/balys/robert/gameofcards/presenters/RegisterPresenter.java
@@ -1,13 +1,16 @@
 package com.sda.balys.robert.gameofcards.presenters;
 
 import android.app.Activity;
+import android.app.ProgressDialog;
 import android.support.annotation.NonNull;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.sda.balys.robert.gameofcards.Utils;
 
 /**
  * Created by RENT on 2017-05-27.
@@ -16,18 +19,53 @@ import com.google.firebase.auth.FirebaseAuth;
 public class RegisterPresenter {
 
     FirebaseAuth firebaseAuth;
+    private ProgressDialog progressDialog;
+
+
+
 
     public RegisterPresenter(){}
 
     public void registerUser(final Activity activity, String email, String password){
         firebaseAuth= FirebaseAuth.getInstance();
 
+
+        if(TextUtils.isEmpty(email)){
+            Utils.showToast(activity,"Enter the mail");
+            return;
+        } else if(!Utils.isEmailVavlid(email)){
+            Utils.showToast(activity,"Enter the valid mail");
+            return;
+        }
+
+        if(TextUtils.isEmpty(password)){
+            Utils.showToast(activity,"Enter the  password ");
+            return;
+        }
+        if(!Utils.isPasswordValid(password)){
+            Utils.showToast(activity,"Enter the valid password");
+            return;
+        }
+
+        if(!Utils.checkInternetConnection(activity)){
+            Utils.showToast(activity,"You have not internet connection");
+            return;
+        }
+
+        progressDialog = new ProgressDialog(activity);
+        progressDialog.setMessage("Registretion...");
+        progressDialog.show();
+
+
         firebaseAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(activity, new OnCompleteListener<AuthResult>() {
             @Override
             public void onComplete(@NonNull Task<AuthResult> task) {
                 if(task.isSuccessful()){
+                    progressDialog.dismiss();
                     Toast.makeText(activity,"Account created",Toast.LENGTH_SHORT).show();
+
                 }else{
+                    progressDialog.dismiss();
                     Toast.makeText(activity,"something went wrong",Toast.LENGTH_SHORT).show();
 
                 }
